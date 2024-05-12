a9c76015c35fe2da628e36da892f3211794dbbae
김주희 <juhee1687@naver.com>
2024-05-12 19:49:27 +0900

    make UI

--- TodoTodo/View/MainView.swift
+++ TodoTodo/View/MainView.swift
@@ -12,6 +12,8 @@ struct MainView: View {
         GeometryReader {_ in
             VStack {
                 TitleView()
+                TodoListView()
+                CalendarView()
             }
         }
     }
@@ -44,6 +46,69 @@ struct TitleView: View {
                     .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 15))
             })
         }
+    }
+}
+
+struct TodoListView: View {
+    var body: some View {
+        
+        List {
+            Section(header: Text("오늘 할일")) {
+                ForEach(1...3, id: \.self) { index in
+                    TodoCell()
+                }
+            }
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+        }
+        .listStyle(GroupedListStyle())
+        .frame(height: 300)
+        .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
+    }
+}
+
+struct TodoCell: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "photo")
+                .font(.system(size: 50))
+                .foregroundColor(.white)
+                .frame(width: 90)
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 0) {
+                
+                Divider().opacity(0)
+                
+                Text("일정 제목")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 10)
+                    .lineLimit(1)
+                
+                Text("일정까지 남은 시간")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+        }
+        .frame(height: 100, alignment: .center)
+        .background(Color.orange)
+        .cornerRadius(15)
+    }
+}
+
+struct CalendarView: View {
+    
+    @State var date = Date()
+    
+    var body: some View {
+        DatePicker(
+            "Start Date",
+            selection: $date,
+            displayedComponents: [.date]
+        )
+        .datePickerStyle(.graphical)
+        .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
     }
 }
 
