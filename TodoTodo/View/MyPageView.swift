e7945c655228149b150ac1862cc8bf86b8bb9e6f
김주희 <>
2024-05-13 10:43:24 +0900

    add BottomNaviView

---
+++ TodoTodo/View/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  TodoTodo
+//
+//  Created by 김주희 on 2024/05/13.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
